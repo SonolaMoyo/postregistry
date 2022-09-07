2a91f98fbb2ce4d1f0434c7b68fc253b4ca7142b
Sonola Moyo <sonolamoyosoluwalorun@gmail.com>
2022-09-07 12:07:16 +0100

    Login form/page implemented

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -68,6 +68,18 @@ def register():
         flash(f'Account created! Username: {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "moyo@veo.com" and form.password.data == 'password':
+            flash("You have been logged in!", 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Check credentials', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
