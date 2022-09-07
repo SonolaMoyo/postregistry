2a999623cb50c47003a4bf9da07f220df3e5295f
Sonola Moyo <sonolamoyosoluwalorun@gmail.com>
2022-09-07 10:17:54 +0100

    environment setup

---
+++ app.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password = db.Column(db.String(36), nullable=False)
+    image = db.Column(db.String(20), nullable=False, default='pic.jpg')
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    posts = db.relationship("Post", backref="author", lazy=True)
+
+    def __repr__(self):
+        return f'User("{self.username}", "{self.email}", "{self.image}")'
+
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
